ffccd8004924cc212e8a6302d4f40320abf5f533
yudha-Dlesmana <yudha.dlesmana@gmail.com>
2025-07-07 22:13:35 +0700

    fix: unfollow button bug suggestion user

--- src/features/Follow/useUnfollow.ts
+++ src/features/Follow/useUnfollow.ts
@@ -13,7 +13,7 @@ export function useUnfollow() {
       toast.success(`unfollowing user`);
       queryClient.invalidateQueries({ queryKey: ["user"] });
       queryClient.invalidateQueries({ queryKey: ["following"] });
-      queryClient.invalidateQueries({ queryKey: ["isFollowed"] });
+      queryClient.invalidateQueries({ queryKey: ["isfollowed"] });
     },
     onError: (error) => {
       if (axios.isAxiosError(error)) {
