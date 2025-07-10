f6591864f51e6c924cf581d362365c23f6328604
yudha-Dlesmana <yudha.dlesmana@gmail.com>
2025-07-10 10:25:04 +0700

    fix: PostTweetType on userPostTweet

--- src/features/PostTweet/usePostTweet.ts
+++ src/features/PostTweet/usePostTweet.ts
@@ -1,14 +1,14 @@
-import { PostTweesFormData } from "@/types/PostTypes";
 import { api } from "@/utils/Apis";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { toast } from "sonner";
+import { PostTweetFormData } from "./TweetsTypes";
 
 export function usePostTweets(onSuccessCallback: () => void) {
   const queryClient = useQueryClient();
 
   const { mutate, isPending } = useMutation({
-    mutationFn: async (data: PostTweesFormData) => {
+    mutationFn: async (data: PostTweetFormData) => {
       await api.post("/tweets", data);
     },
     onSuccess: () => {
